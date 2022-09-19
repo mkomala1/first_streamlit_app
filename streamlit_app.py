148c12cd5c38f75a2fabe929030628766c4616d5
komala <113939859+mkomala1@users.noreply.github.com>
2022-09-19 13:45:34 -0700

    Update streamlit_app.py
    import pandas and adding dataframe fruitlist

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥚 Hardboiled Free Range EGGS')
 streamlit.text('🥑🍞 Avacado toast')
 
 streamlit.header('🍌🍓🥭 Build your own fruit smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
