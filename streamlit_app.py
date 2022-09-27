9cc57e41eefce122941f25d98388e15f27c41e56
komala <113939859+mkomala1@users.noreply.github.com>
2022-09-26 22:15:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+def get_fruitvice_data(this_fruit_choice):
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+    return fruitvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -26,11 +32,12 @@ try:
   if not fruit_choice:
     streamlit.error("Pleae select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruitvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except urlError as e:
   streamlit.error()
 streamlit.stop()
